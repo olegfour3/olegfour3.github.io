042156bbe8e25f11c8df7c904d09a01742274e06
olegfour3 <olegfour3@github.com>
2018-12-06 22:30:39 +0300

    asdasd

--- js/script.js
+++ js/script.js
@@ -73,20 +73,4 @@ $(function(){
                      
     });
     
-    // var mywindow = $(window);
-    // var mypos = mywindow.scrollTop();
-    // var up = false;
-    // var newscroll;
-    // mywindow.scroll(function () {
-    //     newscroll = mywindow.scrollTop();
-    //     if (newscroll > mypos && !up) {
-    //         $('.navbar').stop().slideToggle();
-    //         up = !up;
-    //         console.log(up);
-    //     } else if(newscroll < mypos && up) {
-    //         $('.navbar').stop().slideToggle();
-    //         up = !up;
-    //     }
-    //     mypos = newscroll;
-    // });
 })
